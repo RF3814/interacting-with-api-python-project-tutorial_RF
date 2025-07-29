d60e389e2549be627a64e836eff9fe707b0dcfbc
RF3814 <rodrigofolatre55@gmail.com>
2025-07-29 03:23:35 +0000

    tarea_lista

--- src/app.py
+++ src/app.py
@@ -1,10 +1,28 @@
 import os
 import pandas as pd
-import matplotlib.pyplot as plt
+import seaborn as sns
 from dotenv import load_dotenv
 
 # load the .env file variables
 load_dotenv()
 
+# Get credential values
+client_id = os.environ.get("CLIENT_ID")
+client_secret = os.environ.get("CLIENT_SECRET")
 
-# Spotify API credentials
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+auth_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
+spotify = spotipy.Spotify(auth_manager=auth_manager)
+#5C4PDR4LnhZTbVnKWXuDKD morat
+#2piHiUbXwUNNIvYyIOIUKt francisca
+
+resultados=spotify.artist_top_tracks("5C4PDR4LnhZTbVnKWXuDKD")
+print(resultados)
+#canciones=[]
+#for tracks in resultados['tracks']:
+#    canciones.append({"nombre":tracks['name'],"popularidad":tracks['popularity'],"duracion":tracks['duration_ms']/60000})
+
+
+#print(canciones)
